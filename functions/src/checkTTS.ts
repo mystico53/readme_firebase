0899a7daf5a86c7571bd838f67c711357b3063b8
mystico53 <konkaiser@gmail.com>
2024-04-12 22:43:51 -0700

    added webview share v1

--- functions/src/checkTTS.ts
+++ functions/src/checkTTS.ts
@@ -12,60 +12,83 @@ const db = getFirestore();
 // eslint-disable-next-line require-jsdoc
 function convertGsUrlToHttps(gsUrl: string): string {
   console.log("called convert url");
+  console.log("GS URL:", gsUrl);
   const [, bucketAndPath] = gsUrl.split("gs://");
   const [bucketName, ...pathComponents] = bucketAndPath.split("/");
-  const encodedPath = pathComponents.map((component) => encodeURIComponent(component)).join("/");
-  return `https://storage.googleapis.com/${bucketName}/${encodedPath}`;
+  const encodedPath = pathComponents
+    .map((component) => encodeURIComponent(component))
+    .join("/");
+  const httpsUrl = `https://storage.googleapis.com/${bucketName}/${encodedPath}`;
+  console.log("Converted HTTPS URL:", httpsUrl);
+  return httpsUrl;
 }
 
 export const checkTTS = functions.https.onRequest(async (request, response) => {
   console.log("starting checktts");
   const fileId = request.query.fileId;
+  console.log("File ID:", fileId);
 
   if (!fileId) {
+    console.error("fileId query parameter is missing");
     response.status(400).send("fileId query parameter is required");
     return;
   }
 
   const docRef = db.collection("audioFiles").doc(fileId.toString());
+  console.log("Document reference:", docRef.path);
 
   try {
     const doc = await docRef.get();
+    console.log("Document snapshot:", doc);
 
     if (!doc.exists) {
+      console.error("Document not found for fileId:", fileId);
       response.status(404).send(`No document found for fileId: ${fileId}`);
       return;
     }
 
     const docData = doc.data();
+    console.log("Document data:", docData);
+
     if (!docData) {
+      console.error("Document data is undefined for fileId:", fileId);
       response.status(404).send(`Document data is undefined for fileId: ${fileId}`);
       return;
     }
 
     const operationName = docData.google_tts_operationname;
+    console.log("Operation name:", operationName);
+
     if (!operationName) {
+      console.error("Operation name not found in the document");
       response.status(404).send("Operation name not found in the document.");
       return;
     }
 
     const operation = await textToSpeechClient.checkSynthesizeLongAudioProgress(operationName);
+    console.log("Operation:", operation);
+
     const [, metadata] = await operation.promise();
+    console.log("Operation metadata:", metadata);
 
     await docRef.update({
       google_tts_progress: metadata.progressPercentage,
       done: operation.done,
       status: "ready",
     });
+    console.log("Document updated with progress and status");
 
     // Convert the GCS URI to an HTTPS URL
     console.log("converting url");
     const httpsUrl = docData.gcs_uri ? convertGsUrlToHttps(docData.gcs_uri) : null;
+    console.log("HTTPS URL:", httpsUrl);
 
     if (httpsUrl) {
-      await docRef.update({
-        httpsUrl: httpsUrl,
-      }).then(() => console.log("Document successfully updated with HTTPS URL"))
+      await docRef
+        .update({
+          httpsUrl: httpsUrl,
+        })
+        .then(() => console.log("Document successfully updated with HTTPS URL"))
         .catch((error) => console.error("Error updating document with HTTPS URL:", error));
     }
 
@@ -82,6 +105,8 @@ export const checkTTS = functions.https.onRequest(async (request, response) => {
     response.json(responseObject);
   } catch (error) {
     console.error("Error checking TTS operation status:", error);
-    response.status(500).send(`Error retrieving document: ${error instanceof Error ? error.message : "Unknown error"}`);
+    response.status(500).send(
+      `Error retrieving document: ${error instanceof Error ? error.message : "Unknown error"}`,
+    );
   }
 });
--- functions/src/processRawIntent.ts
+++ functions/src/processRawIntent.ts
@@ -1,34 +1,62 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
 import * as functions from "firebase-functions";
 
+// This function processes the raw intent received in a request
 export const processRawIntent = functions.https.onRequest(async (req, res) => {
   try {
+    console.log("Starting to process raw intent");
     const {text} = req.body;
+
     if (!text) {
       console.error("Text is required");
       res.status(400).send("Text is required");
       return;
     }
 
-    const lines = text.split("\n");
+    console.log(`Received text: ${text}`);
 
-    // eslint-disable-next-line max-len
-    // Check each line for URLs starting with http or https and remove those lines
-    // eslint-disable-next-line max-len
-    const linesWithoutUrls = lines.filter((line: string) => !line.match(/https?:\/\//));
+    let htmlText = text;
+    if (text.includes("\\n\\n")) {
+      const htmlTextArray = htmlText.split("\\n\\n");
 
-    // Count sentences in each line and filter lines with at least two sentences
-    const filteredLines = linesWithoutUrls.filter((line: string) => {
-      const sentenceCount = countSentences(line);
-      return sentenceCount >= 2;
-    });
+      // First filter out items that have 3 words or less
+      const filteredTextArray = htmlTextArray.filter((item: { split: (arg0: RegExp) => { (): any; new(): any; filter: { (arg0: (word: any) => any): { (): any; new(): any; length: number; }; new(): any; }; }; }) => {
+        // Split item into words and check the count
+        return item.split(/\s+/).filter((word) => word).length > 3;
+      });
 
-    // Join the remaining lines with line breaks
-    const processedText = filteredLines.join("\n");
+      // Further filter out items containing '\n' and then join the remaining items
+      htmlText = filteredTextArray.filter((item: string | string[]) => !item.includes("\\n")).join("\n\n");
 
-    console.log("Processed Raw Intent:");
-    console.log(processedText);
+      res.status(200).json({text: htmlText});
+    } else {
+      const lines = text.split("\n");
+      const linesWithoutUrls = lines.filter((line: string) => !line.match(/https?:\/\//));
+      console.log(`Lines after removing URLs: ${linesWithoutUrls.length} lines left`);
 
-    res.status(200).json({text: processedText});
+      // Add a step to filter out lines containing three consecutive dots
+      const linesWithoutDots = linesWithoutUrls.filter((line: string) => !line.includes("..."));
+      console.log(`Lines after removing three dots: ${linesWithoutDots.length} lines left`);
+
+      const filteredLines = linesWithoutDots.filter((line: string) => {
+        const sentenceCount = countSentences(line);
+        console.log(`Line "${line}" has ${sentenceCount} sentences`);
+        return sentenceCount >= 1;
+      });
+
+      // Identify and print duplicate lines
+      const duplicates = findDuplicates(filteredLines);
+      console.log(`Found ${duplicates.length} duplicate lines.`);
+      if (duplicates.length > 0) {
+        console.log("Duplicate lines are:");
+        duplicates.forEach((line) => console.log(line));
+      }
+      const processedText = filteredLines.join("\n");
+      console.log("Processed Raw Intent:");
+      console.log(processedText);
+      res.status(200).json({text: processedText});
+    }
   } catch (error) {
     console.error("Error processing Raw Intent:", error);
     res.status(500).send("An error occurred while processing the Raw Intent");
@@ -36,8 +64,29 @@ export const processRawIntent = functions.https.onRequest(async (req, res) => {
 });
 
 // Helper function to count the number of sentences in a string
-// eslint-disable-next-line require-jsdoc
 function countSentences(text: string): number {
   const sentences = text.match(/[^.!?]+[.!?]/g);
-  return sentences ? sentences.length : 0;
+  const count = sentences ? sentences.length : 0;
+  console.log(`Counted sentences: ${count}`);
+  return count;
+}
+
+// Helper function to find duplicates in an array of strings
+function findDuplicates(lines: string[]): string[] {
+  const duplicates: any[] = [];
+  const lineCounts = new Map();
+
+  lines.forEach((line) => {
+    const currentCount = lineCounts.get(line) || 0;
+    lineCounts.set(line, currentCount + 1);
+  });
+
+  lineCounts.forEach((count, line) => {
+    if (count > 1) {
+      duplicates.push(line);
+    }
+  });
+
+  console.log(`Identified ${duplicates.length} duplicates.`);
+  return duplicates;
 }
